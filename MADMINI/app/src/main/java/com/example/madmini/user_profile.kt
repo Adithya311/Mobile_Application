fedf0dd438038b74196f73a4b1aa729100ea6bd1
IT21193736 <it21193736@my.sliit.lk>
2023-05-02 18:45:59 +0530

    update profile

--- MADMINI/app/src/main/java/com/example/madmini/user_profile.kt
+++ MADMINI/app/src/main/java/com/example/madmini/user_profile.kt
@@ -1,11 +1,26 @@
 package com.example.madmini
-
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class user_profile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_profile)
+
+
+
+
+
+
+
+
+        val btn1: Button = findViewById<Button>(R.id.button2)
+        btn1.setOnClickListener {
+            val intent = Intent(this@user_profile, delete_successfull::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
